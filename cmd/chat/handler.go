34638699d6ae97997561e83e891001edea44d46f
h1deOnBush <287673600@qq.com>
2023-02-09 11:09:50 +0800

    fix: 增加参数校验,调用接口正常时返回成功状态码

--- cmd/chat/handler.go
+++ cmd/chat/handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"tiktok/cmd/chat/service"
 	"tiktok/internal/app"
+	"tiktok/internal/errno"
 	chatpart "tiktok/kitex_gen/chatpart"
 )
 
@@ -12,25 +13,33 @@ type ChatServiceImpl struct{}
 
 // GetChatHistory implements the ChatServiceImpl interface.
 func (s *ChatServiceImpl) GetChatHistory(ctx context.Context, req *chatpart.DouyinMessageChatRequest) (resp *chatpart.DouyinMessageChatResponse, err error) {
-	// TODO: Your code here...
 	resp = new(chatpart.DouyinMessageChatResponse)
+	if req.FromUserId <= 0 || req.ToUserId <= 0 {
+		resp.BaseResp = app.BuildBaseResp(errno.ParamErr)
+		return resp, nil
+	}
 	msgs, err := service.NewQueryChatHistoryService(ctx).QueryChatHistory(req)
 	if err != nil {
 		resp.BaseResp = app.BuildBaseResp(err)
 		return resp, nil
 	}
 	resp.MessageList = msgs
+	resp.BaseResp = app.BuildBaseResp(errno.Success)
 	return resp, nil
 }
 
 // DoMessageAction implements the ChatServiceImpl interface.
 func (s *ChatServiceImpl) DoMessageAction(ctx context.Context, req *chatpart.DouyinMessageActionRequest) (resp *chatpart.DouyinMessageActionResponse, err error) {
-	// TODO: Your code here...
 	resp = new(chatpart.DouyinMessageActionResponse)
+	if req.ActionType != 0 || req.FromUserId <= 0 || req.ToUserId <= 0 {
+		resp.BaseResp = app.BuildBaseResp(errno.ParamErr)
+		return resp, nil
+	}
 	err = service.NewDoMsgActionService(ctx).SendMsg(req)
 	if err != nil {
 		resp.BaseResp = app.BuildBaseResp(err)
 		return resp, nil
 	}
+	resp.BaseResp = app.BuildBaseResp(errno.Success)
 	return resp, nil
 }
